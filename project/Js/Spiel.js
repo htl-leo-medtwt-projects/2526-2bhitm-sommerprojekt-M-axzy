e02bde281e104ca0a95c88d6b93545ceb30ff0c5
Maid Said Velic <m.velic@students.htl-leonding.ac.at>
2026-04-18 15:31:07 +0200

    spielfeld machen für das placne von items, später wird weiter gemacht

--- project/Js/Spiel.js
+++ project/Js/Spiel.js
@@ -1,3 +1,4 @@
+const grid = document.getElementById('grid-overlay');
 const shopContainer = document.getElementById('shop-container');
 const sellTab = document.querySelector('.shop-tabs img[alt="Verkaufen"]');
 const sellScreen = document.getElementById('sell-screen');
@@ -9,6 +10,22 @@ const zrkBtn = document.getElementById('zrk');
 const spielflaeche = document.getElementById('spielflaeche');
 const smellbar = document.getElementById('smellbar');
 const inventory = document.getElementById('inventory');
+
+if (grid) {
+    for (let i = 1; i <= 72; i++) {
+        const feld = document.createElement('div');
+        feld.classList.add('grid-field');
+        
+        feld.onclick = function() {
+            if (feld.innerHTML === "") {
+                const bild = document.createElement('img');
+                bild.src = "../Img/item_stufe1.png";
+                feld.appendChild(bild);
+            }
+        };
+        grid.appendChild(feld);
+    }
+}
 
 if (sellTab) {
     sellTab.onclick = () => {
